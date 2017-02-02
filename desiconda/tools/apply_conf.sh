f77fe42caeb896ebef355f76612b0a66f0c632a1
Theodore Kisner <tskisner.public@gmail.com>
2017-02-01 18:56:47 -0800

    Ignore commented lines

--- desiconda/tools/apply_conf.sh
+++ desiconda/tools/apply_conf.sh
@@ -7,15 +7,19 @@ outfile=$3
 confsub="-e 's#@CONFFILE@#${conffile}#g'"
 
 while IFS='' read -r line || [[ -n "${line}" ]]; do
-    check=$(echo ${line} | sed -e "s#.*=.*#=#")
+    # is this line commented?
+    comment=$(echo "${line}" | cut -c 1)
+    if [ "${comment}" != "#" ]; then
+
+        check=$(echo "${line}" | sed -e "s#.*=.*#=#")
     
-    if [ "x${check}" = "x=" ]; then
-        # get the variable and its value
-        var=$(echo ${line} | sed -e "s#\([^=]*\)=.*#\1#" | awk '{print $1}')
-        val=$(echo ${line} | sed -e "s#[^=]*= *\(.*\)#\1#")
-        
-        # add to list of substitutions
-        confsub="${confsub} -e 's#@${var}@#${val}#g'"
+        if [ "x${check}" = "x=" ]; then
+            # get the variable and its value
+            var=$(echo ${line} | sed -e "s#\([^=]*\)=.*#\1#" | awk '{print $1}')
+            val=$(echo ${line} | sed -e "s#[^=]*= *\(.*\)#\1#")
+            # add to list of substitutions
+            confsub="${confsub} -e 's#@${var}@#${val}#g'"
+        fi
     fi
 
 done < "${conffile}"
